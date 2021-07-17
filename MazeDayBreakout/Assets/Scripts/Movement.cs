625692dc220b2590433e48deeefca9399fd18614
Arthur <Arthur.wei50@gmail.com>
2021-07-17 16:56:53 -0700

    Keyboard Movements & Mouse look Bugfix

--- MazeDayBreakout/Assets/Scripts/Movement.cs
+++ MazeDayBreakout/Assets/Scripts/Movement.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
 
-
-
+    float speed = 100f;
+    public CharacterController characterController;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +16,9 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector3 movement = transform.right * x + transform.forward * y;
+        characterController.Move(movement * speed * Time.deltaTime);
     }
 }
