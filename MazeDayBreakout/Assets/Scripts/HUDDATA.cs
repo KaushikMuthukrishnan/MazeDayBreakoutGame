65ba0b7a2392654443117d0a423dc2b001fda9c9
Arthur <Arthur.wei50@gmail.com>
2021-07-29 17:09:04 -0700

    Health Indicator Setup

--- MazeDayBreakout/Assets/Scripts/HUDDATA.cs
+++ MazeDayBreakout/Assets/Scripts/HUDDATA.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 //instead of a numerical health system, what if we just added some visual FX to simplify it.
 //Like adding blood splatters that fade overtime, and more splatters means less health, kinda like COD
@@ -9,19 +10,20 @@ public class HUDDATA : MonoBehaviour
 {
     // Start is called before the first frame update
     public float health = 100;
-    public float stamina = 100;
-    public float health1;
+    // public float stamina = 100;
     public bool damaged = false;
+    public Text HealthText;
     // Start is called before the first frame update
     void Start()
     {
+        HealthText = GetComponent<Text>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        HealthText.text = "" + health;
         //DECREASE HEALTH when c is pressed
         if (Input.GetKeyDown(KeyCode.D))
         {
@@ -34,11 +36,6 @@ public class HUDDATA : MonoBehaviour
     public void DamageIndicator()
     {
 
-        if (health > health1)
-        {
-            Debug.Log("Health: " + health);
-            health1 = health;
-        }
 
 
     }
