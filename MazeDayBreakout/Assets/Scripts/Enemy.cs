178aac111cfbaecf00a28b103a4933f61bc8a3ac
KaushikMuthukrishnan <kaushikmn113@gmail.com>
2021-07-30 22:35:15 -0700

    fixed shooting bug

--- MazeDayBreakout/Assets/Scripts/Enemy.cs
+++ MazeDayBreakout/Assets/Scripts/Enemy.cs
@@ -14,7 +14,6 @@ public class Enemy : MonoBehaviour
 
     private void Start()
     {
-
         //calls the shoot method at 2 second intervals, almost like a coroutine
         //the random.value is so all bots dont start firing at the same time
         InvokeRepeating("Shoot", Random.value, 1);
@@ -28,12 +27,6 @@ public class Enemy : MonoBehaviour
         laserLight.LookAt(player.position + Vector3.up * 2); //translated up so laser is in cameras
     }
 
-
-/*    private void OnDrawGizmos()
-    {
-        Gizmos.DrawSphere(player.position, 2f);
-
-    }*/
     private void Shoot()
     {
         if (health <= 0)
@@ -44,15 +37,15 @@ public class Enemy : MonoBehaviour
         }
         //finds a random point in the vicinity of player so robots arent lazer focused on player with each shot
         //aka adds randomness to shots
-        var randPoint = Random.insideUnitSphere * 3f; // *3 increases the radius of the sphere of randomness around player
-        var targetDir = randPoint + player.position - muzzle.position; //gets direction to be used for Phys.Raycast()
+        var randPoint = Random.insideUnitSphere * 3f; // * 3 increases the radius of the sphere of randomness around player
+        var targetDir = randPoint + player.position - muzzle.position; //gets direction argument to be used for Phys.Raycast()
             
         fireBurst.Play();
 
         //checks to see if player was hit on raycast, causes damage if so
-        if (Physics.Raycast(muzzle.position, targetDir, out RaycastHit hit, 30, LayerMask.GetMask("Player", "EnvironmentalObjs")) && hit.transform.gameObject != null);
+        if (Physics.Raycast(muzzle.position, targetDir, out RaycastHit hit, 30, LayerMask.GetMask("Player", "EnvironmentalObjs"))
+            && hit.transform != null)
         {
-            //Weird null transform glitch
             int layer = hit.transform.gameObject.layer;
             if (layer == LayerMask.NameToLayer("EnvironmentalObjs"))
             {
