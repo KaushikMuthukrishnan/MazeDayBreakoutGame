7096561386db2264b1334c1b31422b3f8bea4b32
Arthur <Arthur.wei50@gmail.com>
2021-08-01 17:05:10 -0700

    More Door Stuff

--- MazeDayBreakout/Assets/Scripts/DoorSlide.cs
+++ MazeDayBreakout/Assets/Scripts/DoorSlide.cs
@@ -5,24 +5,42 @@ using UnityEngine;
 public class DoorSlide : MonoBehaviour
 {
     public GameObject Future_Door;
+    public bool neverDone;
+    public bool triggerEntered;
 
-
-    // Update is called once per frame
+    void Start()
+    {
+        triggerEntered = false;
+        neverDone = true;
+    }
     void Update()
     {
-        //when the player press space animation will start
-        if (Input.GetKeyDown(KeyCode.Space))
+
+        if (neverDone && Input.GetKeyDown(KeyCode.Space) && triggerEntered)
         {
             Future_Door.GetComponent<Animator>().SetTrigger("Trigger");
 
-            Debug.Log("Slide");
-
+            neverDone = false;
         }
-
-
-
-
-
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            triggerEntered = true;
+            // Debug.Log("Trigger entered");
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (neverDone == false)
+        {
+            Future_Door.GetComponent<Animator>().SetTrigger("Close");
+            neverDone = true;
+            // Debug.Log("Trigger exited");
+        }
+    }
+
 
 }
