3ebe19397b20b35a47e8414d44020aa71c57bd33
KaushikMuthukrishnan <kaushikmn113@gmail.com>
2021-08-02 00:15:12 -0700

    adding some email scripts

---
+++ MazeDayBreakout/Assets/Scripts/EmailingSequence/MailApp.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MailApp : MonoBehaviour
+{
+
+    private void OnMouseOver()
+    {
+        //for highlighting the object
+    }
+
+    private void OnMouseDown()
+    {
+        //open app
+    }
+}
