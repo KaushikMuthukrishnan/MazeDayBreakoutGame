a77fad0919bcca1394f0866c4ce44cdc58a0de6c
KaushikMuthukrishnan <kaushikmn113@gmail.com>
2021-08-02 22:11:06 -0700

    going back

--- MazeDayBreakout/Assets/Scripts/GunShoot.cs
+++ MazeDayBreakout/Assets/Scripts/GunShoot.cs
@@ -9,6 +9,7 @@ public class GunShoot : MonoBehaviour
     public Camera cam;
     //name changed cuz it was giving an error
     public GameObject Gun;
+    public static bool gunEnabled = false;
 
     private void Update()
     {
@@ -27,37 +28,16 @@ public class GunShoot : MonoBehaviour
 
     void Shoot()
     {
-        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out RaycastHit hit))
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        if (gunEnabled && Physics.Raycast(ray, out RaycastHit hit))
         {
-            //check if the guns parernt is the called "GunPlaceHolder" to see if the gun is in the players hands 
-            if (Gun.transform.parent.gameObject == GameObject.Find("GunPlaceHolder"))
-            {
-
-                Debug.Log(hit.point);
-                GameObject laser = GameObject.Instantiate(shotPrefab, transform.position, transform.rotation);
-                laser.transform.LookAt(hit.point);
-                laser.GetComponent<ShotBehavior>().setTarget(hit.point);
-                //TODO: Need to fix a bug where the laser goes through the wall. 
-                Destroy(laser, 1f);
-
-
-
-
-            }
-
+            Debug.Log(hit.point);
+            GameObject laser = Instantiate(shotPrefab, transform.position, transform.rotation);
+            laser.transform.LookAt(hit.point);
+            laser.GetComponent<ShotBehavior>().setTarget(hit.point);
+            //TODO: Need to fix a bug where the laser goes through the wall. 
+            Destroy(laser, 1f);
         }
-
-
-        //For the lazer, I would check out the Line renderer cuz its much simpler to turn on and off than to instantiate new gameojects
-        //it takes a while to get used to tho
-
-        //? uh, I dont know what you mean by turn on and off. I thought we were doing as the same as the star wars blasters where its just a lazer bullet.
-
-
-
-
-
     }
 }
 
