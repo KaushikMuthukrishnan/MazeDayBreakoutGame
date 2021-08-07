bb9338f5bab7373c63e968c3b6dafd21eba3c5d8
anshulw101 <anshoops@gmail.com>
2021-08-06 21:37:33 -0700

    working on some dialogue and shi

---
+++ MazeDayBreakout/Assets/dialoguemanager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialoguemanager : MonoBehaviour
+{
+
+    public Queue<string> sentences;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+
+        
+    }
+
+    
+}
