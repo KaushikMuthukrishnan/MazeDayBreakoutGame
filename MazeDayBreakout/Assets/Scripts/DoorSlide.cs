1203a3899b8aed5ccf1e7f3f5c30942a8a78bd83
KaushikMuthukrishnan <kaushikmn113@gmail.com>
2021-08-08 16:04:43 -0700

    hands toggle complete

--- MazeDayBreakout/Assets/Scripts/DoorSlide.cs
+++ MazeDayBreakout/Assets/Scripts/DoorSlide.cs
@@ -32,26 +32,24 @@ public class DoorSlide : MonoBehaviour
 
         if (neverDone && triggerEntered && Input.GetKeyDown(KeyCode.Space))
         {
-        
             StartCoroutine(OpenDoorAnim());
-
-            //audio
-            slideNoise.pitch = 1.0f;
-            slideNoise.Play();
-
-            Future_Door.GetComponent<Animator>().SetTrigger("Trigger");
-            text.text = ""; //this is so the tootltip disappears once action is executed;
-            neverDone = false;
-
         }
     }
 
     IEnumerator OpenDoorAnim()
     {
         text.text = ""; //this is so the tootltip disappears once action is executed;
-        punchAnim.Play(GunShoot.gunEnabled ? "PunchLeft" : "PunchRight"); //punches left if gun is held
-        yield return new WaitForSeconds(1.5f);
-        Future_Door.GetComponent<Animator>().SetTrigger("Trigger");
+        if (!GunShoot.gunEnabled)
+        {
+            punchAnim.Play("PunchRight"); 
+            yield return new WaitForSeconds(1.5f);
+        }
+
+        //audio
+        slideNoise.pitch = 1.0f;
+        slideNoise.Play();
+
+        Future_Door.GetComponent<Animator>().SetTrigger("Trigger"); 
         neverDone = false;
         yield break;
     }
@@ -61,7 +59,7 @@ public class DoorSlide : MonoBehaviour
         if (other.gameObject.tag == "Player")
         {
             triggerEntered = true;
-            text.text = "Press SPACE to Punch Open Door";
+            text.text = GunShoot.gunEnabled? "Press SPACE to Open Door" : "Press SPACE to Punch Open Door";
             // Debug.Log("Trigger entered");
         }
     }
--- MazeDayBreakout/Assets/Scripts/GunShoot.cs
+++ MazeDayBreakout/Assets/Scripts/GunShoot.cs
@@ -8,10 +8,9 @@ public class GunShoot : MonoBehaviour
     public GameObject shotPrefab;
     public Camera cam;
     //name changed cuz it was giving an error
-    public GameObject Gun;
     public static bool gunEnabled = false;
 
-    public AudioSource gunShot;
+    private AudioSource gunShot;
 
     private void Start()
     {
@@ -20,41 +19,33 @@ public class GunShoot : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (gunEnabled && !Movement.frozen && TimeStamp <= Time.time && Input.GetMouseButtonDown(0)) 
+        //checks for if gun is enabled, and if not in UI scene, and reload speed, and if pressing SHOOT
         {
+            gunShot.Play();
 
-            if (TimeStamp <= Time.time)
-            {
-                gunShot.Play();
-
-                //firerates
-                TimeStamp = Time.time + 1.5f;
-                Shoot();
-
-            }
+            //firerates
+            TimeStamp = Time.time + 1.5f;
+            Shoot();
         }
     }
 
     void Shoot()
     {
-        if (gunEnabled && !Movement.frozen) //checks for if gun is enabled, and if not in UI scene
-        {
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            Physics.Raycast(ray, out RaycastHit hit);
-            GameObject laser = Instantiate(shotPrefab, transform.position, transform.rotation);
-            laser.transform.LookAt(hit.point != null ? hit.point : ray.origin);
-            laser.GetComponent<ShotBehavior>().setTarget(hit.point != null ? hit.point : ray.origin);
-            hit.collider?.GetComponentInParent<Enemy>()?.TakeDamage(60); // inflicts damage on enemy
-            //TODO ADJUST DAMAGE RATE
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(ray, out RaycastHit hit);
+        GameObject laser = Instantiate(shotPrefab, transform.position, transform.rotation);
+        laser.transform.LookAt(hit.point != null ? hit.point : ray.origin);
+        laser.GetComponent<ShotBehavior>().setTarget(hit.point != null ? hit.point : ray.origin);
+        hit.collider?.GetComponentInParent<Enemy>()?.TakeDamage(60); // inflicts damage on enemy
+        //TODO ADJUST DAMAGE RATE
 
-            //New bug is when the shot is aimed into empty space, the shot goes towards one specific spot
-            //Cant figure out how to fix it but we shouldnt worry abt it for now, its very minor, since most of our map will have colliders and 
-            //structure so hit.point will almost never be null
+        //New bug is when the shot is aimed into empty space, the shot goes towards one specific spot
+        //Cant figure out how to fix it but we shouldnt worry abt it for now, its very minor, since most of our map will have colliders and 
+        //structure so hit.point will almost never be null
 
-            //TODO: Need to fix a bug where the laser goes through the wall. 
-            //Destroy(laser, 1f); laser is auto destroyed in source code upon reaching target
-
-        }
+        //TODO: Need to fix a bug where the laser goes through the wall. 
+        //Destroy(laser, 1f); laser is auto destroyed in source code upon reaching target
     }
 }
 
