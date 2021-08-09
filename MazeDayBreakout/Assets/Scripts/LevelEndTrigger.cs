a526a1a90e2657fa4e95ae4d04d99e7986e327b6
KaushikMuthukrishnan <kaushikmn113@gmail.com>
2021-08-08 22:45:48 -0700

    updating scene

--- MazeDayBreakout/Assets/Scripts/LevelEndTrigger.cs
+++ MazeDayBreakout/Assets/Scripts/LevelEndTrigger.cs
@@ -1,17 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelEndTrigger : MonoBehaviour
 {
+    public TextMeshProUGUI tooltip;
 
     private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            tooltip.text = "Press SPACE to Escape!";
             if (Input.GetKeyDown(KeyCode.Space))
                 SceneManager.LoadScene(2);
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            tooltip.text = "";
     }
 }
