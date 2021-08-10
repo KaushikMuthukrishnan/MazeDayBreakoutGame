956c650a2b20f84699274c78520a9b33d6de529f
KaushikMuthukrishnan <kaushikmn113@gmail.com>
2021-08-09 19:44:04 -0700

    begin subs work

---
+++ MazeDayBreakout/Assets/Scripts/Subtitles/SubsManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SubsManager : MonoBehaviour
+{
+    public TextMeshProUGUI subs;
+    public InputField nameField;
+
+    public static bool enteredName = false;
+    public static bool punchedDoor = false;
+    public static bool enemiesSpawned = false;
+    public static bool firstWaveKilled = false;
+    public static bool enteredServerRoom = false;
+    public static bool sentMail = false;
+    public static bool secondWaveKilled = false;
+    private void Start()
+    {
+
+    }
+
+
+}
