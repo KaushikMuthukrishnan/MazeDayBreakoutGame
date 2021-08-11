cf71bceadc67926ed06218b3eb3d28679886a517
KaushikMuthukrishnan <kaushikmn113@gmail.com>
2021-08-11 15:53:23 -0700

    player respawn works

--- MazeDayBreakout/Assets/Scripts/HUDDATA.cs
+++ MazeDayBreakout/Assets/Scripts/HUDDATA.cs
@@ -5,48 +5,34 @@ using UnityEngine.UI;
 
 public class HUDDATA : MonoBehaviour
 {
-    public float health = 100;
+    private float health = 100;
     public Text HealthText;
     public Slider HealthSlider;
-    public Vector3 Spawnpoint;
-    public GameObject player;
+    public Transform Spawnpoint;
+    private Transform player;
 
     
     void Start(){
 
-        Spawnpoint = player.transform.position;
-        player = GameObject.FindGameObjectWithTag("Player");
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     void Update()
     {
         if (health <= 0)
         {
- 
-        //   player.transform.position = new Vector3(20, 50, 100);
-        //   player.transform.rotation = Quaternion.Euler(0, 0, 0);
-        //   player.transform.position= new vector3(Spawnpoint.x, Spawnpoint.y, Spawnpoint.z);
-        player.transform.position = Spawnpoint;
+            player.SetPositionAndRotation(Spawnpoint.position, Spawnpoint.rotation);
             HealthSlider.value=100;
             health = 100;
         }
-        HealthText.text = "" + health + "%";
-        DamageIndicator();
+
+        UpdateDamage();
         
     }
-    public void DamageIndicator()
+    public void UpdateDamage()
     {
-//if g is pressed subtract health
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            health -= 20;
-        }
-
-        if (HealthSlider.value <= 0){
-            health = 0;
-        }
+        HealthText.text = "" + health + "%";
         HealthSlider.value = health;
-     
     }
 
     //This method is to be accessed by the enemy scripts so that if their shooting raycast hits the player,
