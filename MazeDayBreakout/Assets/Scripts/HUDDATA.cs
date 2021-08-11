1fb609c2161c49d77a61407a12d7c9ed2d7cc198
KaushikMuthukrishnan <kaushikmn113@gmail.com>
2021-08-11 15:46:42 -0700

    player respawn works

--- MazeDayBreakout/Assets/Scripts/HUDDATA.cs
+++ MazeDayBreakout/Assets/Scripts/HUDDATA.cs
@@ -5,52 +5,36 @@ using UnityEngine.UI;
 
 public class HUDDATA : MonoBehaviour
 {
-    public float health = 100;
+    private float health = 100;
     public Text HealthText;
     public Slider HealthSlider;
-    public Vector3 Spawnpoint;
-    public GameObject player;
+    public Transform Spawnpoint;
+    private Transform player;
 
     
-    void Start(){
-
-        Spawnpoint = player.transform.position;
-        player = GameObject.FindGameObjectWithTag("Player");
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     void Update()
     {
         if (health <= 0)
         {
- 
-        //   player.transform.position = new Vector3(20, 50, 100);
-        //   player.transform.rotation = Quaternion.Euler(0, 0, 0);
-        //   player.transform.position= new vector3(Spawnpoint.x, Spawnpoint.y, Spawnpoint.z);
-        player.transform.position = Spawnpoint;
-            HealthSlider.value=100;
+            player.SetPositionAndRotation(Spawnpoint.position, Spawnpoint.rotation);
+            HealthSlider.value = 100;
             health = 100;
         }
-        HealthText.text = "" + health + "%";
-        DamageIndicator();
+        UpdateHealth();
         
     }
-    public void DamageIndicator()
+    public void UpdateHealth()
     {
-//if g is pressed subtract health
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            health -= 20;
-        }
-
-        if (HealthSlider.value <= 0){
-            health = 0;
-        }
         HealthSlider.value = health;
-     
+        HealthText.text = health + "%";
     }
 
-    //This method is to be accessed by the enemy scripts so that if their shooting raycast hits the player,
-    //they can call this method attached to the player
+    //this method subtracts player health
     public void TakeDamage(int damageTaken)
     {
         health -= damageTaken;
